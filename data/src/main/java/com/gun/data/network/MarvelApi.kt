038401210929d3964f005d1ed1daa58c100b2ad3
Gun-HelloWorld <gun.helloworld@gmail.com>
2023-05-21 00:21:22 +0900

    [오류수정] 잘못된 API Path 수정

--- data/src/main/java/com/gun/data/network/MarvelApi.kt
+++ data/src/main/java/com/gun/data/network/MarvelApi.kt
@@ -10,25 +10,25 @@ import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface MarvelApi {
-    @GET("/v1/public/character/{characterId}")
+    @GET("/v1/public/characters/{characterId}")
     suspend fun getCharacter(@Path("characterId") characterId: Int): CharactersDto
 
     @GET("/v1/public/characters")
     suspend fun getCharacterList(@Query("offset") offset: Int, @Query("limit") limit: Int): CharactersDto
 
-    @GET("/v1/public/comic/{comicId}")
+    @GET("/v1/public/comics/{comicId}")
     suspend fun getComic(@Path("comicId") comicId: Int): ComicDto
 
     @GET("/v1/public/comics")
     suspend fun getComicList(@Query("offset") offset: Int, @Query("limit") limit: Int): ComicDto
 
-    @GET("/v1/public/creator/{creatorId}")
+    @GET("/v1/public/creators/{creatorId}")
     suspend fun getCreator(@Path("creatorId") creatorId: Int): CreatorDto
 
     @GET("/v1/public/creators")
     suspend fun getCreatorList(@Query("offset") offset: Int, @Query("limit") limit: Int): CreatorDto
 
-    @GET("/v1/public/event/{eventId}")
+    @GET("/v1/public/events/{eventId}")
     suspend fun getEvent(@Path("eventId") eventId: Int): EventDto
 
     @GET("/v1/public/events")
