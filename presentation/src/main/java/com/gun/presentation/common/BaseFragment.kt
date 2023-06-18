f7092774547683f30fdb5e49bd6e170fae5921bd
Gun-HelloWorld <gun.helloworld@gmail.com>
2023-06-18 19:42:55 +0900

    [화면수정] 프래그먼트 onViewCreated() 시점에 기본적으로 풀스크린 플래그 제거하도록 수정

--- presentation/src/main/java/com/gun/presentation/common/BaseFragment.kt
+++ presentation/src/main/java/com/gun/presentation/common/BaseFragment.kt
@@ -1,11 +1,17 @@
 package com.gun.presentation.common
 
+import android.os.Bundle
 import android.view.View
 import android.view.animation.AlphaAnimation
 import androidx.fragment.app.Fragment
 import com.gun.presentation.ui.main.MainActivity
 
 abstract class BaseFragment: Fragment() {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setWindowLayoutNoLimit(false)
+    }
 
     fun setWindowLayoutNoLimit(needExpand: Boolean) {
         (requireActivity() as MainActivity).setWindowLayoutNoLimit(needExpand)
