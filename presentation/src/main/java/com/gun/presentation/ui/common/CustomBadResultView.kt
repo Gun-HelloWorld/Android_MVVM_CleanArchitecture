6aec2a528e081f1c14686d0ebac867c86b332e39
Gun-HelloWorld <gun.helloworld@gmail.com>
2023-07-22 00:47:49 +0900

    [이슈수정] 검색화면에서 존재하지 않은 검색어 입력 후 에러 화면 표시된 상태에서 다른 프래그먼트 이동 후 돌아올 시 Text 표시되지 않는 현상 수정

--- presentation/src/main/java/com/gun/presentation/ui/common/CustomBadResultView.kt
+++ presentation/src/main/java/com/gun/presentation/ui/common/CustomBadResultView.kt
@@ -5,6 +5,7 @@ import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.FrameLayout
+import androidx.core.view.doOnPreDraw
 import com.gun.mvvm_cleanarchitecture.R
 import com.gun.mvvm_cleanarchitecture.databinding.ViewCustomBadResultBinding
 
@@ -29,11 +30,13 @@ class CustomBadResultView @JvmOverloads constructor(
     }
 
     fun show(badResultType: BadResultType) {
-        with (binding) {
-            tvTitle.text = getTitleFromBadResultType(badResultType)
-            tvMessage.text = getMessageFromBadResultType(badResultType)
-            btnRetry.visibility = if (badResultType == ResultEmptyType) View.GONE else View.VISIBLE
-            visibility = View.VISIBLE
+        with(binding) {
+            doOnPreDraw {
+                tvTitle.text = getTitleFromBadResultType(badResultType)
+                tvMessage.text = getMessageFromBadResultType(badResultType)
+                btnRetry.visibility = if (badResultType == ResultEmptyType) View.GONE else View.VISIBLE
+                visibility = View.VISIBLE
+            }
         }
     }
 
